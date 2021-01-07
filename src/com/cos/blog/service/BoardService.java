42a9ebd1e35f4940315594ec2c0504502af9894b
이재철 <lee@ijaecheol-ui-MacBookPro.local>
2021-01-07 11:10:08 +0900

    글쓰기 완료

--- src/com/cos/blog/service/BoardService.java
+++ src/com/cos/blog/service/BoardService.java
@@ -1,5 +1,17 @@
 package com.cos.blog.service;
 
-public class BoardService {
+import com.cos.blog.domain.board.BoardDao;
+import com.cos.blog.domain.board.dto.SaveReqDto;
+import com.cos.blog.domain.user.UserDao;
 
+public class BoardService {
+	
+	private BoardDao boardDao;
+	
+	public BoardService() {
+		this.boardDao = new BoardDao();
+	}
+	public int 글쓰기(SaveReqDto dto) {
+		return boardDao.save(dto);
+	}
 }
